6f1625abb0736d683692276813c9f1adde142438
Ikenna <ikenna@kali.Ikenna-PC>
2023-01-23 00:27:11 -0500

    v2 Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 the api file
 """
 
-from os import getenv
+from os import environ
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -14,16 +14,17 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_dataBase():
-    """a method that calls storage.close"""
+def close_dataBase(e):
+    """tear down app context"""
+    
     storage.close()
 
 
 if __name__ == '__main__':
-    host=getenv('HBNB_API_HOST')
-    port=getenv('HBNB_API_PORT')
+    host=environ.get('HBNB_API_HOST')
+    port=environ.get('HBNB_API_PORT')
     if not host:
         host = '0.0.0.0'
     if not port:
         port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
